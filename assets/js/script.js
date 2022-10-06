67c5aa958cd24ffcf4ad1cda5b0bed0d1825e03a
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-10-06 18:45:22 +0000

    Display additin question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame (gameType);
             }
         })
+
+
     }
+
+    runGame ("addition");
 })
 
 /**
@@ -18,11 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     // Creats two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 
 }
 
@@ -42,7 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion () {
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1
+    document.getElementById("operator").textContent = "+"
+    document.getElementById("operand2").textContent = operand2
 
 }
 
